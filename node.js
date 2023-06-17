57827ad945dac134690ab333e59e893cdd26e6d8
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-06-17 12:42:19 +0100

    feat: updates

--- node.js
+++ node.js
@@ -4,8 +4,9 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['prettier'],
+  extends: ['plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': [
--- react.js
+++ react.js
@@ -4,8 +4,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['prettier'],
+  extends: ['plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': [
