3a42ea33b207c69a0efc2d2bce95913f52ba572c
Daniel Sousa <jdaniel.asousa@gmail.com>
2023-07-28 19:38:39 +0100

    feat(wip): init repos with lerna

---
+++ packages/prettier-config/README.md
@@ -0,0 +1,11 @@
+# `prettier-config`
+
+> TODO: description
+
+## Usage
+
+```
+const prettierConfig = require('prettier-config');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
