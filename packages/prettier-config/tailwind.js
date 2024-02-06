5558fbc413dae66322a55f7368d6e48c365e761f
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-02-06 11:44:58 +0000

    refactor: use prettier config on package.json

--- packages/prettier-config/tailwind.js
+++ packages/prettier-config/tailwind.js
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  ...require("./index"),
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindFunctions: ["clsx", "cn", "twMerge", "cva"],
+  ...require('./index'),
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx', 'cn', 'twMerge', 'cva'],
 };
