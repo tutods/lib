188280b43e518dad3ceae0286588cce9aab00c12
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-02-06 11:28:06 +0000

    feat: add prettier config for tailwind and cleanup

---
+++ packages/prettier-config/tailwind.js
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  ...require("./index"),
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cn", "twMerge", "cva"],
+};
